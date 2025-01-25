5e8267f1f50bd2c1ce5dd809372a1f54539bee50
Andrei Monichev <vyroud@gmail.com>
2025-01-25 22:16:55 +0300

    added test

--- conftest.py
+++ conftest.py
@@ -16,7 +16,7 @@ def load_env():
     load_dotenv()
 
 
-@pytest.fixture(scope='session',
+@pytest.fixture(scope='function',
                 params=[('9.0', 'android', 'Motorola Moto G7 Play')],
                 ids=['base'],
                 autouse=True)
